456b790c8645f6a71009430520b798da9dfe8031
KhadijaHameed01 <taibakausar469@gmail.com>
2025-01-10 17:23:39 +0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import { join } from 'path';
+import cors from 'cors';
+import { readFile } from 'fs';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+// Get the __dirname equivalent in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);  // Correct this line
+
+// Now you can use __dirname like in CommonJS
+console.log(__dirname);
+
+const app = express();
+const PORT = 5000;
+
+// Middleware
+app.use(cors());
+
+// Route to serve blogs from blogData.json
+app.get('/blogs', (req, res) => {
+  const filePath = join(__dirname, 'api', 'blogData.json');
+  readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error reading blogData.json:', err);
+      res.status(500).json({ error: 'Unable to fetch blog posts' });
+    } else {
+      res.json(JSON.parse(data)); 
+    }
+  });
+});
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Blog API! Use /blogs to fetch blog posts.');
+});
+
+// Starting server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
